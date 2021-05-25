2358410390643bbfdbff1687ce14eedd0253ce48
ana-vela <ana@anavela.net>
2021-05-24 20:58:03 -0600

    compare text

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const { response } = require("express");
 const saltRounds = 10;
 const plainText = "ReskillAmericans123";
 const port = 4000;
-let pass = "";
+const inputText = "SampleTextPw";
 
 app.use(express.json());
 
 bcrypt.hash(plainText, saltRounds).then((hash) => {
   console.log(hash);
+ 
+  bcrypt.compare(inputText, hash).then(result => {
+    console.log(result);
+})
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
